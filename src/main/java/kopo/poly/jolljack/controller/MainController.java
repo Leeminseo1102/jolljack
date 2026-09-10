940143639c673188dffd87158462c78ac51c05d2
이민서 <lmsjtsljh@naver.com>
2026-09-10 10:09:59 +0900

    기존 메인 컨트롤러는 세션의 유저아이디만 가져왔는데 지금은 세션 아이디를 가져와 널이 아닐시 RegionWeatherService의 getRegionWeatherProc 로 전달후 값 온걸 main.html로 전달

--- src/main/java/kopo/poly/jolljack/controller/MainController.java
+++ src/main/java/kopo/poly/jolljack/controller/MainController.java
@@ -1,9 +1,12 @@
 package kopo.poly.jolljack.controller;
 
 import jakarta.servlet.http.HttpSession;
+import kopo.poly.jolljack.dto.RegionWeatherDTO;
+import kopo.poly.jolljack.service.IRegionWeatherService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,11 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/main")
 public class MainController {
 
+    private final IRegionWeatherService regionWeatherService;
+
     /**
      * 메인 페이지
      */
     @GetMapping("/view")
-    public String mainPage(HttpSession session) {
+    public String mainPage(HttpSession session, Model model) {
 
         log.info("{}.mainPage Start!", this.getClass().getName());
 
@@ -28,6 +33,21 @@ public class MainController {
             log.info("session userId : {}", userId);
             log.info("session userName : {}", userName);
 
+            if (userId != null) {
+
+                try {
+                    RegionWeatherDTO rDTO =
+                            regionWeatherService.getRegionWeatherProc(userId);
+
+                    model.addAttribute("weather", rDTO);
+
+                } catch (Exception e) {
+                    log.info("{}.mainPage Weather Error : {}",
+                            this.getClass().getName(), e.getMessage(), e);
+
+                    model.addAttribute("weather", null);
+                }
+            }
             log.info("{}.mainPage End!", this.getClass().getName());
 
             return "main/main";
